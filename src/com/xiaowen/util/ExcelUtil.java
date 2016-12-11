e0c49696410db0b5aaed8fc926db30f0b90a3754
xiaowen <1583310645@qq.com>
2016-12-11 19:45:13 +0800

    Excel工具类

---
+++ src/com/xiaowen/util/ExcelUtil.java
@@ -0,0 +1,93 @@
+package com.xiaowen.util;
+import java.io.InputStream;
+import java.sql.ResultSet;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+/**
+ * Excel工具类
+ * @author xiaowen
+ * 2016年12月11日
+ */
+public class ExcelUtil {
+
+	public static void fillExcelData(ResultSet rs,Workbook wb,String[] headers)throws Exception{
+		int rowIndex=0;
+		Sheet sheet=wb.createSheet();
+		Row row=sheet.createRow(rowIndex++);
+		for(int i=0;i<headers.length;i++){
+			row.createCell(i).setCellValue(headers[i]);
+		}
+		while(rs.next()){
+			row=sheet.createRow(rowIndex++);
+			for(int i=0;i<headers.length;i++){
+				row.createCell(i).setCellValue(rs.getObject(i+1).toString());
+			}
+		}
+	}
+	
+	public static Workbook fillExcelDataWithTemplate(ResultSet rs,String templateFileName)throws Exception{
+		InputStream inp=ExcelUtil.class.getResourceAsStream("/template/"+templateFileName);
+		POIFSFileSystem fs=new POIFSFileSystem(inp);
+		Workbook wb=new HSSFWorkbook(fs);
+		Sheet sheet=wb.getSheetAt(0);
+		// 获取列数
+		int cellNums=sheet.getRow(0).getLastCellNum();
+		int rowIndex=1;
+		while(rs.next()){
+			Row row=sheet.createRow(rowIndex++);
+			for(int i=0;i<cellNums;i++){
+				row.createCell(i).setCellValue(rs.getObject(i+1).toString());
+			}
+		}
+		return wb;
+	}
+	
+	public static String formatCell(HSSFCell hssfCell){
+		if(hssfCell==null){
+			return "";
+		}else{
+			if(hssfCell.getCellType()==HSSFCell.CELL_TYPE_BOOLEAN){
+				return String.valueOf(hssfCell.getBooleanCellValue());
+			}else if(hssfCell.getCellType()==HSSFCell.CELL_TYPE_NUMERIC){
+				return String.valueOf(hssfCell.getNumericCellValue());
+			}else{
+				return String.valueOf(hssfCell.getStringCellValue());
+			}
+		}
+	}
+	
+	
+	public static String getCell(HSSFCell cell) {
+		DecimalFormat df = new DecimalFormat("#");
+		if (cell == null)
+			return "";
+		switch (cell.getCellType()) {
+		case HSSFCell.CELL_TYPE_NUMERIC:
+			if(HSSFDateUtil.isCellDateFormatted(cell)){
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				return sdf.format(HSSFDateUtil.getJavaDate(cell.getNumericCellValue())).toString();
+			}
+			return df.format(cell.getNumericCellValue());
+		case HSSFCell.CELL_TYPE_STRING:
+			System.out.println(cell.getStringCellValue());
+			return cell.getStringCellValue();
+		case HSSFCell.CELL_TYPE_FORMULA:
+			return cell.getCellFormula();
+		case HSSFCell.CELL_TYPE_BLANK:
+			return "";
+		case HSSFCell.CELL_TYPE_BOOLEAN:
+			return cell.getBooleanCellValue() + "";
+		case HSSFCell.CELL_TYPE_ERROR:
+			return cell.getErrorCellValue() + "";
+		}
+		return "";
+	}
+}
